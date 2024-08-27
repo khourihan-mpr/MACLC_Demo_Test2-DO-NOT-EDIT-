b0fb1bd73ac36f0a5bbec247c685e556ffcf9c33
Kerianne Hourihan <khourihan@mathematica-mpr.com>
2024-08-27 14:32:45 -0400

    Moved __init__.py

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     name="MACLC_Test_Functions",
     python_requires=">=3.8",
     packages=find_packages(),
+    setup_requires=["wheel"],
     version=__version__,
     description="This is a test package for demo purposes",
-    author="Kerianne Hourihan (Mathematica)"
+    author="Kerianne Hourihan (Mathematica)",
+    license="CC0 1.0 Universal"
 )
