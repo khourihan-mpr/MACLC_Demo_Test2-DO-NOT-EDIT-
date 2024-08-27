f7a68b71aeda93bb73c29e933756e8e835d6fa00
Kerianne Hourihan <khourihan@mathematica-mpr.com>
2024-08-27 14:03:27 -0400

    Updated package name

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 from Test_pkg.Test_module import __version__
 
 setup(
-    name="My Package",
+    name="MACLC_Test_Functions",
     python_requires=">=3.8",
     packages=find_packages(),
     version=__version__,
-    description="This is a description of my package",
-    author="My name"
+    description="This is a test package for demo purposes",
+    author="Kerianne Hourihan (Mathematica)"
 )
